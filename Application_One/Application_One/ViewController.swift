daeeb7456d7057ec89aece1687b413866f37419c
bm181354 <bm181354@bu.edu>
2017-07-20 19:19:58 -0400

    Application1

---
+++ Application_One/Application_One/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Application_One
+//
+//  Created by Hari maharjan on 4/13/17.
+//  Copyright © 2017 Biken Maharjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
